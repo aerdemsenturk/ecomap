5dddc8bc0e8d8dc4a09836cc0ff4c9ef761bdfb4
A. Erdem Şentürk <birtakimseyler@gmail.com>
2019-09-20 01:25:23 +0200

    taxonomy-actor yakında kalkacak. Bunun yerine post type geldi.

--- themes/map-child/taxonomy-actor.php
+++ themes/map-child/taxonomy-actor.php
@@ -46,6 +46,8 @@ $container   = get_theme_mod( 'understrap_container_type' );
 		
 		<p><?php global $wp_query; echo $wp_query->post_count; ?> adet sonuç görüntüleniyor</p>
 
+
+
 	</div><!-- Container end -->
 
 	<div class="card-block">
@@ -55,12 +57,10 @@ $container   = get_theme_mod( 'understrap_container_type' );
 		<div class="card">
 			<div class="card-header" >
 				<div class="row">
-
-					<div class="col-1 p-1 pb-2">
-						<a href="#gm-map-1" title="Haritada göster" data-toggle="tooltip" data-placement="right" onclick="window.scrollTo(0,0);frames['gm-map-1'].GeoMashup.clickMarker('<?php the_ID()?>')"><div class="show_on_map">✹</div></a>				
+					<div class="col-12 col-lg-1 p-1 pb-2">
+						<a class="show_on_map" title="Haritada göster" data-toggle="tooltip" href="#gm-map-1" onclick="window.scrollTo(0,0);frames['gm-map-1'].GeoMashup.clickMarker('<?php the_ID()?>')">✹</a>				
 					</div>
-
-					<div class="col-1 p-1 pb-2">
+					<div class="col-12 col-lg-1 p-1 pb-2">
 						<?php
 						$case_type = get_the_terms( $post->ID,  'case_type' );
 						if ( ! empty( $case_type ) ) {
@@ -72,16 +72,14 @@ $container   = get_theme_mod( 'understrap_container_type' );
 						}
 						?>
 					</div>
-
-					<div class="col-4 p-0 pt-1">
+					<div class="col-12 col-lg-4 p-1">
 						<small><?php echo get_the_date('Y'); ?></small>
 						<a role="tab" id="<?php the_ID(); ?>" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php the_ID(); ?>"
 						aria-expanded="false" aria-controls="collapse<?php the_ID(); ?>">
 						<h4><?php the_title(); ?></h4>
 						</a>
 					</div>
-
-					<div class="col-3 text-left p-1 pb-2">
+					<div class="col-12 col-lg-3 text-left p-1 pb-2">
 						<?php
 						$commodity_terms = get_the_terms( $post->ID,  'commodity' );
 						if ( ! empty( $commodity_terms ) ) {
@@ -93,8 +91,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 						}
 						?>
 					</div>
-
-					<div class="col-3 text-left p-1 pb-2">
+					<div class="col-12 col-lg-3 text-left p-1 pb-2">
 						<?php
 						$impact_terms = get_the_terms( $post->ID,  'impact' );
 						if ( ! empty( $impact_terms ) ) {
@@ -106,64 +103,103 @@ $container   = get_theme_mod( 'understrap_container_type' );
 						}
 						?>
 					</div>
-
 				</div>
 			</div>
-		
+
 			<div id="collapse<?php the_ID(); ?>" class="collapse" role="tabpanel" aria-labelledby="<?php the_ID(); ?>">
 				<div class="card-block">
-
-						<div class="row">
-							<div class="col-6 pb-2 pt-2 ">
-								<?php the_excerpt(); ?>
-							</div>
-							<div class="col-3 pb-2">
-							<?php	
-								$types = get_field('actor_pro');
-								foreach($types as $type) {
-									$term_link = get_term_link( $type );
-									$actor_type = get_field('actor_type', 'actor_' . $type->term_id . '' );
-
-									if ( has_term( $type->term_id, 'actor')) {
-										echo '<ul class="list-group list-group-flush">';
-										echo '<li class="list-group-item"><small><a href="' . esc_url( $term_link ) . '">' . $type->name . '</a> - <i>' . $actor_type . '</i></small></li>';
-										echo '</ul>';
-									}
-								} 
-							?>
-							</div>
-							<div class="col-3 pb-2">
-							<?php	
-								$types = get_field('actor_contra');
-								foreach($types as $type) {
-									$term_link = get_term_link( $type );
-									$actor_type = get_field('actor_type', 'actor_' . $type->term_id . '' );
-
-									if ( has_term( $type->term_id, 'actor')) {
-										echo '<ul class="list-group list-group-flush">';
-										echo '<li class="list-group-item"><small><a href="' . esc_url( $term_link ) . '">' . $type->name . '</a> - <i>' . $actor_type . '</i></small></li>';
-										echo '</ul>';
-									}
-								} 
-							?>
-							</div>
-
-							<div class="col-3">
-								<?php if ( function_exists( 'has_post_thumbnail') and has_post_thumbnail() ) : ?>
-								<?php the_post_thumbnail( array(900,900) ); ?>
-								<?php endif; ?>
-							</div>
+					<div class="row">
+						<div class="col-12 col-lg-6 pb-2 pt-2">
+							<?php the_excerpt(); ?>
 						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 mt-5 mb-3">
+							<?php
+							
+								if (have_rows('actor_pro')) {
+									while (have_rows('actor_pro')) {
 
+										the_row();
+										$finances = get_sub_field('finance');
+										$goverments = get_sub_field('goverment');
+										$companies = get_sub_field('company');
+
+										if( $goverments ):
+
+										foreach ($goverments as $goverment) {
+
+											echo '<ul>';
+											echo '<li><a href="' . get_term_link( $goverment ) . '">' . $goverment->name . '</a> - <i> Goverment </i></li></li>';
+											echo '</ul>';
+
+										}
+
+										endif;
+										if( $companies ):
+
+										foreach ($companies as $company) {
+
+											echo '<ul>';
+											echo '<li><a href="' . get_term_link( $company ) . '">' . $company->name . '</a> - <i> Company </i></li></li>';
+											echo '</ul>';
+
+										}
+
+										endif;
+										if( $finances ):
+
+										foreach ($finances as $finance) {
+
+											echo '<ul>';
+											echo '<li><a href="' . get_term_link( $finance ) . '">' . $finance->name . '</a> - <i> Finance </i></li></li>';
+											echo '</ul>';
+					
+										}
+										endif;
+									}
+								}
+
+							?>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 mt-5 mb-3">
+							<?php
+								
+								if (have_rows('actor_contra')) {
+									while (have_rows('actor_contra')) {
+
+										the_row();
+										$non_goverments = get_sub_field('non-goverment');
+
+
+										if( $non_goverments ):
+
+										foreach ($non_goverments as $non_goverment) {
+
+											echo '<ul>';
+											echo '<li><a href="' . get_term_link( $non_goverment ) . '">' . $non_goverment->name . '</a> - <i> Goverment </i></li></li>';
+											echo '</ul>';
+
+										}
+
+										endif;
+								
+									}
+								}
+
+							?>
+						</div>
+						<div class="col-12 col-lg-3">
+							<?php if ( function_exists( 'has_post_thumbnail') and has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( array(900,900) ); ?>
+							<?php endif; ?>
+						</div>
+					</div>
 				</div>
 			</div>
-
 		</div>
 
 		<div class="clearfix"></div>
 
-		<?php endwhile;
-		else : ?>
+		<?php endwhile; else : ?>
 		<h4><?php _e('No posts'); ?></h4>
 		<?php endif; ?>
 
@@ -178,17 +214,22 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 <script type="text/javascript">
 
-	//TOGGLE
+	//Tooltip 
 	jQuery(function () {
-	jQuery('[data-toggle="tooltip"]').tooltip()
+		jQuery('[data-toggle="tooltip"]').tooltip(
+			{
+            placement: 'right', 
+            animation: false, // https://github.com/twbs/bootstrap/issues/21607#issuecomment-309634023
+			}
+		)
 	})
 
-	// https://stackoverflow.com/questions/35992900/bootstrap-accordion-scroll-to-top-of-active-open-accordion-on-click
+	// TOGGLE
 	jQuery('.collapse').on('shown.bs.collapse', function(e) {
-	var $card = jQuery(this).closest('.card');
-	jQuery('html,body').animate({
-		scrollTop: $card.offset().top
-	}, 400);
+		var $card = jQuery(this).closest('.card');
+		jQuery('html,body').animate({
+			scrollTop: $card.offset().top // https://stackoverflow.com/questions/35992900/bootstrap-accordion-scroll-to-top-of-active-open-accordion-on-click
+		}, 400);
 	});
 
 </script>
